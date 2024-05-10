9ac80c3cd8fa6e931be45d8e48381efeec76e758
Av4nz <cinxzaned2003@gmail.com>
2024-05-10 16:27:30 +0700

    second update

--- app.js
+++ app.js
@@ -7,21 +7,12 @@ const port = 3000;
 
 const prisma = new PrismaClient()
 
-// PostgreSQL configuration
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'commuFundDB',
-//     password: 'admin',
-//     port: 5433,
-// });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/frontend/index.html');
 });
 
 app.post("/addUserOG", async(req, res) => {
@@ -33,28 +24,18 @@ app.post("/addUserOG", async(req, res) => {
                 deskripsi: desk_og
             }    
         });
-        res.status(201).json({ message: "User OG created successfully", user: result });
+        res.send(result)
     } catch (e) {
         console.error("errorrr", e)
         res.status(500).json({ error: "Internal server error" });
     }
 })
 
-// app.post('/submit', (req, res) => {
-//     const { nama_organisasi, desc_organisasi } = req.body;
-
-//     // Insert data into PostgreSQL
-//     pool.query('INSERT INTO organisasi (nama_organisasi, deskripsi_organisasi) VALUES ($1, $2)', [nama_organisasi, desc_organisasi], (err, result) => {
-//         if (err) {
-//             console.error('Error executing query', err);
-//             res.status(500).send('Error inserting data');
-//         } else {
-//             console.log('Data inserted successfully');
-//             res.status(200).send('Data inserted successfully');
-//         }
-//     });
-// });
+app.get('/addUserOG', (req, res) => {
+    console.log("get addUserOG")
+    res.send()
+})
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
